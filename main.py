5b7bb5e91fb25477f58bbf4a05cce8e7818c78d8
Suryansh Sukhija <85603828+suryansh101010@users.noreply.github.com>
2022-10-08 20:50:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+from math import ceil
 import time
 from wonderwords import RandomSentence
 
@@ -9,65 +10,106 @@ def main(scr):
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     redblack = curses.color_pair(1)
     greenblack = curses.color_pair(2)
-    str = "Press any key to start or press esc to exit"
+    str = "Press any key to start or press esc to exit" #First screen
     middle_x = int(round((curses.COLS/2)-20)) #x coord
     horz = int(middle_x - len(str)/20)
     vert = int(round((curses.LINES/2)-2)) #y coord
     scr.addstr(vert,horz,f"{str}")
+    str = "Pressing any number key will give the corrosponding no. of sentences" #First screen
+    middle_x = int(round((curses.COLS/2)-31)) #x coord
+    horz = int(middle_x - len(str)/20)
+    vert = int(round((curses.LINES/2)-2)) #y coord
+    scr.addstr(vert+1,horz,f"{str}")
     scr.refresh()
     inp = scr.getch()
     if inp == 27:
-        exit()
-    else:
+        exit() #exit on esc
+    else: #countinue on any other key
         while True:
-            count = -1
+            count = -1 #setting parameters
             correct = 0
             wrong = 0
             scr.clear()
-            s = RandomSentence()
-            text = s.sentence() +" "+s.sentence()+" "+s.sentence()
+            s = RandomSentence() #putting the module class in a var
+            try:
+                no = int(chr(inp))
+                text = ""
+                for i in range(no):
+                    text += s.sentence() + " "
+            except Exception as e:
+                text = s.sentence() +" "+s.sentence()+" "+s.sentence() #generation of 3 random sentences
             scr.addstr(f"{text}")
             scr.refresh()
-            wpm = 0
-            start_time = time.time()
+            scr.move(0,0) #bringing cursor as starting
+            wpm = 0 #setting more parameters
+            timer = 0
+            vertical = 0
             while True:
                 exit = False
                 try:
-                    if count == len(text)-1:
-                        raise IndexError
-                    time_elapsed = max(time.time() - start_time, 1)
+                    if count == len(text)-1: #finishing when user hits the end of sentences
+                        raise IndexError #using Index error to provide an ending
+                    if count+2 > curses.COLS: #support for multiple lines
+                        vertical = 1
+                    elif count+2 > 2*curses.COLS:
+                        vertical = 2
+                    elif count+2 > 3*curses.COLS:
+                        vertical = 3
                     scr.refresh()
                     char = scr.getch()
+                    skip = False
+                    if not timer:
+                        start_time = time.time() #time for WPM and time elapsed :shrug:
+                        timer+=1
+                    time_elapsed = max(time.time() - start_time, 1)
                     count += 1
-                    if char == 8:
-                        if count > 1:
-                            count -= 1
+                    if char == 8: #handling backspaces
+                        if count > 1 and count < 3:
+                            count = 2
+                            scr.move(0,count+1)
+                        if count > 2:
+                            count -= 2
                             wrong -= 1
+                            scr.move(vertical,count-(vertical*curses.COLS)+4)
                         else:
-                            count = 0
-                    elif char == 27:
+                            count = -1
+                            scr.move(0,count+1)
+                    elif char == 27: #handling esc
                         exit = True
                         break
-                    ord_char = char
-                    char = chr(char)
-                    if char == text[count]:
-                        scr.addstr(0,count,f"{char}",greenblack)
-                        correct += 1
-                    else:
-                        if char != "enter":
-                            scr.addstr(0,count,f"{char}",redblack)
-                            wrong += 1
-                    wpm = ((correct + wrong)/(time_elapsed))*10
-                    scr.addstr(1,0,f"Time elapsed: {round(time_elapsed)}s, WPM: {round(wpm)}")
+                    elif char == 260: #handling left arrow key
+                        count -= 2
+                        skip = True
+                        scr.move(0,count+1)
+                    elif char == 261: #handling right arrow key
+                        count += 0
+                        skip = True
+                        scr.move(0,count+1)
+                    if not skip:
+                        char = chr(char) #converts input from ASCII value to normal key
+                        if char == text[count]: #comarision with actual text
+                            scr.addstr(vertical,count-(vertical*curses.COLS),f"{char}",greenblack)
+                            scr.move(vertical,count-(vertical*curses.COLS)+1)
+                            correct += 1
+                        else:
+                            if char != "enter":
+                                scr.addstr(vertical,count-(vertical*curses.COLS),f"{char}",redblack) #Mistakes on wrong keypresses
+                                scr.move(vertical,count-(vertical*curses.COLS)+1)
+                                wrong += 1
+                    wpm = (correct/time_elapsed)*12 #calculating WPM
+                    actual_vert = ceil((len(text)/curses.COLS))
+                    scr.addstr(actual_vert,0,f"Time elapsed: {round(time_elapsed)}s, WPM: {round(wpm)}") #Info to be shown while typing
+                    scr.move(vertical,count-(vertical*curses.COLS)+1)
                 except IndexError:
-                    scr.addstr(0,count,f"\naccuracy = {round((correct/(correct+wrong))*100)}%\ntime = {round(time_elapsed)}\nWPM = {round(wpm)}\npress any key to play again or press esc to exit",greenblack)
-                    inp = scr.getch()
-                    if inp == 27:
+                    scr.addstr(actual_vert,0,f"time = {round(time_elapsed)}\nWPM = {round(wpm)}\npress any key to play again or press esc to exit")#Info after done typing
+                    inp = scr.getch() #handling key presses after done
+                    if inp == 27: #esc key handling
                         exit()
                     else:
                         break
-                except Exception as e:
-                    print(e)
+                except Exception as e: #in case of any other exception (mainly for debugging)
+                    #print(e)
+                    pass
             if exit:
                 break
 
